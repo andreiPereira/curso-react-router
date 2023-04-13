02886302a6c1d37b4eb2761c785dcd0689fa3d96
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-04-13 10:51:08 -0300

    2 - Maximizando o poder do router

--- src/paginas/cadastro/LayoutBaseCadastro.jsx
+++ src/paginas/cadastro/LayoutBaseCadastro.jsx
@@ -7,8 +7,7 @@ const LayoutBaseCadastro = () => {
         <Row justify="center">
             <Col xxx={6} xxl={6} xl={6} lg={6} md={8} sm={12} style={{ margin: '80px 0' }}>
                 <Card>
-                    <Outlet /> 
-                    {/* Outlet é componente do react router que coloca os childrens no local certo */}
+                    <Outlet /> {/* Outlet é componente do react router que coloca os childrens no local certo */}
                 </Card>
             </Col>
         </Row>
--- src/paginas/cadastro/SelecaoCliente.jsx
+++ src/paginas/cadastro/SelecaoCliente.jsx
@@ -2,9 +2,10 @@ import { Col, Row } from "react-grid-system"
 import { Tipografia } from "../../componentes/Tipografia/Tipografia"
 import cliente from './assets/cliente.png'
 import freelancer from './assets/freela.png'
+import { Link } from "../../componentes/Link/Link"
 
 const SelecaoCliente = () => {
-    return(
+    return (
         <>
             <Tipografia variante='h1' componente='h1'>
                 Crie seu cadastro
@@ -15,11 +16,25 @@ const SelecaoCliente = () => {
             <Row>
                 <Col md={6} sm={12}>
                     <img src={cliente} alt='' />
+                    <Tipografia variante='body' componente='body'>
+                        Sou cliente e preciso de um freela !
+                    </Tipografia>
                 </Col>
                 <Col md={6} sm={12}>
                     <img src={freelancer} alt='' />
+                    <Tipografia variante='body' componente='body'>
+                        Sou freela e preciso de um cliente !
+                    </Tipografia>
                 </Col>
             </Row>
+            <div style={{ textAlign: 'center' }}>
+                <Tipografia variante="body2" componente="body2">
+                    Já tem conta?
+                </Tipografia>
+                <p>
+                    <Link variante="secundaria">Faça login!</Link>
+                </p>
+            </div>
         </>
     )
 }
